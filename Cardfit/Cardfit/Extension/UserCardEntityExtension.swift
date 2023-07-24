db7c8586b1ae33d040575dd560dbba19d8b4c217
domb <woon4910@gmail.com>
2023-07-24 13:46:33 +0900

    update: modify FirebaseManager (add query couse of fetching limitted cards)

--- Cardfit/Cardfit/Extension/UserCardEntityExtension.swift
+++ Cardfit/Cardfit/Extension/UserCardEntityExtension.swift
@@ -20,8 +20,10 @@ extension UserCardEntity {
         
         let cards = cardEntities.map { cardEntity in
             let benefit = try? JSONDecoder().decode(Benefits.self, from: cardEntity.benefit ?? Data())
-            return Card(id: Int(cardEntity.cardNumber!), cardName: cardEntity.cardName, cardNumber: cardEntity.cardNumber, cardImageURL: cardEntity.cardImageURL, imageData: cardEntity.imageData, domesticAnnualFee: cardEntity.domesticAnnualFee, requiredPreviousMonthUsage: cardEntity.requiredPreviousMonthUsage, mainBenefit: cardEntity.mainBenefit, company: cardEntity.company, benefit: benefit)
+            return Card(id: Int(cardEntity.cardNumber!), identifier: cardEntity.identifier!, cardName: cardEntity.cardName, cardNumber: cardEntity.cardNumber!, cardImageURL: cardEntity.cardImageURL, imageData: cardEntity.imageData, domesticAnnualFee: cardEntity.domesticAnnualFee, requiredPreviousMonthUsage: cardEntity.requiredPreviousMonthUsage, mainBenefit: cardEntity.mainBenefit, company: cardEntity.company, benefit: benefit)
         }
         return cards
     }
 }
+
+
